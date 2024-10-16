0db8009c1c87759f3b100d4bb9e607ed5cb2ed17
LZ27@proton.me <157941859+Newfies@users.noreply.github.com>
2024-10-16 18:09:48 -0500

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:Run-Process
+cls
+title GsLibrary Selenium Automation Template - Run Script
+echo Starting the script...
+echo.
+cls
+echo [Selenium] Press Any Key To Stop The script.
+echo.
+start /b node server.js
+pause > nul
+echo.
+taskkill /f /im node.exe
+echo.
+choice /c YN /m "Do you want to restart the script (Y/N)?"
+if errorlevel 2 goto Exit-Process
+if errorlevel 1 goto Run-Process
+
+:Exit-Process
+echo Script stopped. Exiting now...
+timeout 5 >nul
